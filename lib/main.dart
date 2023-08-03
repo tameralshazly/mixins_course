079fca85ad3f8384d0c76d58f7951114293327fd
tameralshazly <tameralshazly@yahoo.com>
2023-08-04 01:11:25 +0400

    step-04

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,50 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'dart:developer' as devtools show log;
+import 'dart:io';
 
-void testIt() {
-  final cat = Cat();
-  cat.run();
-  cat.speed = 20;
-  cat.run();
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+void testIt() async {
+  final people = await const GetPeople().getString();
+  people.log();
 }
 
 extension Log on Object {
   void log() => devtools.log(toString());
+}
+
+extension GetOnUri on Object {
+  Future<HttpClientResponse> getUrl(
+    String url,
+  ) =>
+      HttpClient()
+          .getUrl(
+            Uri.parse(url),
+          )
+          .then(
+            (request) => request.close(),
+          );
+}
+
+mixin CanMakeGetCall {
+  String get url;
+
+  @useResult
+  Future<String> getString() => getUrl(
+        url,
+      ).then((response) => response
+          .transform(
+            utf8.decoder,
+          )
+          .join());
+}
+
+@immutable
+class GetPeople with CanMakeGetCall {
+  const GetPeople();
+  @override
+  String get url => 'http://172.20.10.5:5500/apis/people.json';
 }
 
 void main() {
@@ -36,21 +71,4 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-}
-
-mixin CanRun on Animal {
-  int get speed;
-
-  void run() {
-    'Running at the speed of $speed'.log();
-  }
-}
-
-class Cat extends Animal with CanRun {
-  @override
-  int speed = 10;
-}
-
-abstract class Animal {
-  const Animal();
 }
