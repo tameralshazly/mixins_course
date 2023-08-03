e4405b60716305791ee65f605900387fab96a66a
tameralshazly <tameralshazly@yahoo.com>
2023-08-03 23:52:16 +0400

    step-02

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
+
+void testIt() {
+  final cat = Cat();
+  cat.run();
+  cat.speed = 20;
+  cat.run();
+}
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
 
 void main() {
   runApp(
@@ -17,10 +29,24 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    testIt();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
     );
   }
+}
+
+mixin CanRun {
+  int get speed;
+
+  void run() {
+    'Running at the speed of $speed'.log();
+  }
+}
+
+class Cat with CanRun {
+  @override
+  int speed = 10;
 }
